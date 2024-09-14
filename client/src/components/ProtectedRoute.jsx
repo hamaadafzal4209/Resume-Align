0886a7df9586538e9bd41ca11ca31df5084e9a78
FrontendFlair <hamaadafzal404@gmail.com>
2024-09-13 21:23:15 -0700

    commit

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -1,7 +1,7 @@
 import { Navigate } from 'react-router-dom';
 import { useAuth } from '@clerk/clerk-react';
 
-const ProtectedRoute = ({ element }) => {
+const ProtectedRoute = ({ children }) => {
   const { isSignedIn } = useAuth();
 
   // If the user is not signed in, redirect to the sign-in page
@@ -9,8 +9,8 @@ const ProtectedRoute = ({ element }) => {
     return <Navigate to="/sign-in" />;
   }
 
-  // If the user is signed in, render the element
-  return element;
+  // If the user is signed in, render the children
+  return children;
 };
 
 export default ProtectedRoute;
